f1694c0e15ee45545e3e5f9d0a1a54bfb600282e
sabermahmud <sabermahmud01@gmail.com>
2026-07-17 03:46:45 +0300

    first commit. add operation done in a function.

---
+++ function.js
@@ -0,0 +1,37 @@
+// Coding Challenge (Easy)  আজ ১০টি Practice:
+
+// Challenge 1
+// দুইটি সংখ্যা যোগ করে Return করো।
+function add (num1 ,num2){
+    const sum = num1 + num2;
+    return sum
+}
+ add (25, 5);
+
+
+// Challenge 2
+// তিনটি সংখ্যা যোগ করে Return করো।
+
+// Challenge 3
+// একটি সংখ্যা Even নাকি Odd Return করো।
+
+// Challenge 4
+// দুইটি সংখ্যার মধ্যে বড়টি Return করো।
+
+// Challenge 5
+// তিনটি সংখ্যার মধ্যে বড়টি Return করো।
+
+// Challenge 6
+// একটি Array-এর Sum Return করো।
+
+// Challenge 7
+// একটি Array-এর Average Return করো।
+
+// Challenge 8
+// একটি String-এর Length Return করো।
+
+// Challenge 9
+// একটি সংখ্যার Square Return করো।
+
+// Challenge 10
+// একটি সংখ্যার Cube Return করো।
